48f43b20b68387c5ea8a17bdd3d742bc9223eda2
hemmerson <hemmerson.rosa@gmail.com>
2025-02-22 22:50:37 -0300

    Initial commit

---
+++ src/main/java/com/hemmersonrosa/EntregaExpressa.java
@@ -0,0 +1,13 @@
+package com.hemmersonrosa;
+
+public class EntregaExpressa implements EntregaStrategy {
+    @Override
+    public String tempoEntrega() {
+        return "";
+    }
+
+    @Override
+    public String custoEntrega() {
+        return "";
+    }
+}
---
+++ src/main/java/com/hemmersonrosa/EntregaNormal.java
@@ -0,0 +1,13 @@
+package com.hemmersonrosa;
+
+public class EntregaNormal implements EntregaStrategy {
+    @Override
+    public String tempoEntrega() {
+        return "";
+    }
+
+    @Override
+    public String custoEntrega() {
+        return "";
+    }
+}
